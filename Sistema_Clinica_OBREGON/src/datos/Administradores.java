5d4830a56c70f4d8bfe59e0f8433e1133d30a79a
cesarobregon <131396060+cesarobregon@users.noreply.github.com>
2024-03-14 13:24:24 -0300

    act 2

---
+++ Sistema_Clinica_OBREGON/src/datos/Administradores.java
@@ -0,0 +1,226 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+import com.mysql.jdbc.Statement;
+import entidades.Administrador;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import miselaneos.Conexion;
+
+/**
+ *
+ * @author cesar
+ */
+public class Administradores extends Conexion{
+    public Administradores() {
+        setTabla("administrador");
+    }
+    
+    /**
+     * Metodo que recibe un parametro tipo Object y lo REGISTRA en la base de
+     * datos en la tabla especialidades
+     *
+     * @param o
+     * @return true=registra, false=presencia de error
+     */
+    @Override
+    public boolean isNew(Object o) {
+        Administrador adm = (Administrador) o;
+        boolean isOk = false;
+        try {
+            //Creo un Objeto tipo Statament (st), que es necesario para procesar sentencias SQL
+            Statement st = (Statement) this.getCn().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+            //Del Objeto Statement genero otro Objeto tipo ResultSet que almacena los resultados
+            //de la consulta sql.
+            //En este caso resultados en blanco porque en la consulta no hay registros con id=-1
+            //Esto se hace para luego crear un nuevo registro en blanco
+            ResultSet rs = st.executeQuery("SELECT * FROM " + getTabla() + " WHERE id=-1");
+
+            //Creo un nuevo registro en blanco para luego ir actualizando cada campo del registro
+            rs.moveToInsertRow();
+
+            //rs.updateInt("id", p.getId()); // como el campo de id es autoincremental no se hace mención
+            //Actualizo los campos del nuevo registro con los datos que recibo del Objeto Especialidad 
+            rs.updateString("usuario", adm.getUsuario());
+            rs.updateString("clave", adm.getClave());
+
+            //Por ultimo para que se termine de crear un registro en la base de datos/tabla, ejecuto el metodo insertRow
+            rs.insertRow();
+
+            //Cierro ResultSet (rs) y Statament (st), es indispensable NO Olvidarse de cerrar
+            //estos objetos. De no ser asi, NO se registrara que se creo un registro
+            rs.close();
+            st.close();
+
+            //Por Ultimo pongo a la bandera del metodo en true para indicar que no ha ocurrido ningun error
+            //y el metodo tubo exito
+            //Nota: es importante definir a lo ultimo del codigo esta bandera indicando que todos los pasos
+            //fueron realizados con exito
+            isOk = true;
+
+        } catch (SQLException e) {
+            //Si hay alguna excepción en algun momento relacionado con algun metodo mal escrito o mal ejecutado, o bien
+            //problemas en la red para ejecutar los metodos se sucede esta seccion de codigo.
+            System.out.println(e.getMessage());
+        } finally {
+            return isOk;
+        }
+    }
+
+    /**
+     * Metodo que recibe como parametro un Objeto tipo Especialidad y lo
+     * ACTUALIZA en la base de datos en la tabla especialidades
+     *
+     * @param o
+     * @return true=registra, false=presencia de error
+     */
+    @Override
+    public boolean isUpdate(Object o) {
+        Administrador adm = (Administrador) o;
+        boolean isOk = false;
+        try {
+            Statement st = (Statement) this.getCn().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+            ResultSet rs = st.executeQuery("SELECT * FROM " + getTabla() + " WHERE id=" + adm.getId());
+
+            //Metodo next() que indica si puedo ir al nuevo resultado del ResultSet, indicando que tengo un registro.
+            if (rs.next()) {
+                //Me ubico en el resultado y actualizo los campos
+                rs.updateString("usuario", adm.getUsuario());
+                rs.updateString("clave", adm.getClave());
+
+                //Metodo para atualizar resultado(registro) en la base de datos/tabla
+                rs.updateRow();
+                isOk = true;
+            }
+            rs.close();
+            st.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getErrorCode());
+        } finally {
+            return isOk;
+        }
+    }
+
+    /**
+     * Metodo que recibe como parametro un Objeto tipo Especialidad y lo BORRA
+     * en la base de datos en la tabla especialidades
+     *
+     * @param l
+     * @return true=registra, false=presencia de error
+     */
+    @Override
+    public boolean isDelete(Object o) {
+        Administrador adm = (Administrador) o;
+        boolean isOk = false;
+        try {
+            Statement st = (Statement) this.getCn().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+            ResultSet rs = st.executeQuery("SELECT * FROM " + getTabla() + " WHERE id=" + adm.getId());
+
+            if (rs.next()) {
+                //Al ejecutar el metodo deleteRow() confirmo los campos en la base de datos/tabla
+                rs.deleteRow();
+                isOk = true;
+            }
+            rs.close();
+            st.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return isOk;
+        }
+    }
+
+    /**
+     * Metodo que BORRA TODAS las Especialidades en la base de datos
+     *
+     * @return true=registra, false=presencia de error
+     */
+    @Override
+    public boolean isDeleteAll() {
+        boolean isOk = false;
+        try {
+            Statement st = (Statement) this.getCn().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+            isOk = st.executeUpdate("DELETE FROM " + getTabla()) > 0;
+
+            st.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            return isOk;
+        }
+    }
+
+    /**
+     * Metodo que recibo una consulta SQL en String y retorna un ArrayList de
+     * Especialidad
+     *
+     * @param query
+     * @return ArrayList
+     */
+    @Override
+    public ArrayList list(String query) {
+        ArrayList<Administrador> list = new ArrayList<Administrador>();
+        try {
+            Statement st = (Statement) this.getCn().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                Administrador admin = new Administrador(rs.getInt("id"), rs.getString("usuario"), rs.getString("clave"));
+                list.add(admin);
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            return list;
+        }
+    }
+
+    /**
+     * Metodo que recibo codigo de Especialidad y retorna un Objeto de tipo
+     * Especialidad, sino encuentra la especialidad el Objeto que retorna es
+     * nulo (null)
+     *
+     * @param id
+     * @return Location
+     */
+    public Administrador getAdministrador(int id) {
+        Administrador adm = null;
+        //Saber si me puedo Conectar
+        if (isOkConexion()) {
+            //Genero consulta SQL
+            String query = "SELECT * FROM " + getTabla() + " WHERE id=" + id;
+            //Reutilizo metodo de especialidadesList con SQL en busqueda de especialidades
+            ArrayList<Administrador> administradoresList = list(query);
+            isCloseConexion();//Cierro Conexion
+
+            //Pregunto si el ArrayList en respuesta del metodo list(***) tiene un Objeto
+            if (administradoresList.size() == 1) {
+                //Si tiene acceso a la primer posicion
+                Administrador aList = administradoresList.get(0);
+                //hago una nueva instancia de obra(ObraSocial) con el valor de la posicion 0
+                adm = new Administrador(aList.getId(), aList.getUsuario(), aList.getClave());
+            }
+        }
+        //Retorno el valor del Objeto obra
+        return adm;
+    }
+    
+}
